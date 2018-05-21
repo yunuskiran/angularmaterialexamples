c97883ffb8dfa6034866346970156eab851397b9
Yunus KIRAN <yunus.kiran@besiktas.li>
2018-05-21 23:34:49 +0300

    initial

---
+++ src/app/mynav/mynav.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'mynav',
+  templateUrl: './mynav.component.html',
+  styleUrls: ['./mynav.component.css']
+})
+export class MynavComponent {
+  isHandset: Observable<BreakpointState> = this.breakpointObserver.observe(Breakpoints.Handset);
+  constructor(private breakpointObserver: BreakpointObserver) {}
+}
