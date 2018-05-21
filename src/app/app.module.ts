745703fda07e9b84ea4080a4f186322bc579c5de
Yunus KIRAN <yunus.kiran@besiktas.li>
2018-05-21 23:34:05 +0300

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MynavComponent } from './mynav/mynav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule } from '@angular/material';
+import { BlogComponent } from './blog/blog.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { DashComponent } from './dash/dash.component';
+
+const appRoutes:Routes=[
+  {
+    path:'home',component:HomeComponent
+  },
+  {
+    path:'blog',component:BlogComponent
+  },
+  {
+    path:'about',component:AboutComponent
+  },
+  {
+    path:'dashboard',component:DashComponent
+  }  
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MynavComponent,
+    BlogComponent,
+    AboutComponent,
+    HomeComponent,
+    DashComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
